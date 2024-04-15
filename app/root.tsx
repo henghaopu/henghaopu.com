c214e7bc5bc9b43221ccbb516163034017c1ecbe
Heng-Hao Pu <henghaopu@gmail.com>
2024-04-14 23:50:43 -0500

    Links to public files

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,22 @@
+import { LinksFunction } from '@remix-run/node';
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react';
 
-export function Layout({ children }: { children: React.ReactNode }) {
+/**
+ * Reference: https://remix.run/docs/en/main/route/links
+ * Add links to the page by exporting a links function
+ * Returns an array of link objects that specify the favicon for the HTML document
+ */
+export const links: LinksFunction = () => {
+  return [{ rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }];
+};
+
+export function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <head>
